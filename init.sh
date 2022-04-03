31412ecb05ba59656ef062ae37ef1d69afb39cf2
Ismael Casimpan Jr <ismael.angelo@casimpan.com>
2022-04-03 12:50:41 +0800

    Add option to AUTOSTART_PROJECT per #18.

--- init.sh
+++ init.sh
@@ -16,7 +16,6 @@ fi
 ## Read project config
 ##
 PROJECT_ALIAS=${1:-"$(basename $(pwd))"}
-export PROJECT_ROOTDIR="${DOCKTIE_COMMON_PARENTDIR}/${PROJECT_ALIAS}" ## for docktie-ext use
 
 CONF_DIR="$DOCKTIE_DIR/conf"
 ENV_FILE="$CONF_DIR/${PROJECT_ALIAS}_env"
@@ -32,7 +31,7 @@ fi
 export DOCKTIE_ENV="$ENV_FILE"
 . $DOCKTIE_ENV
 
-## Docker container(s) running...
+export PROJECT_ROOTDIR="${DOCKTIE_COMMON_PARENTDIR}/${PROJECT_ALIAS}" ## for docktie-ext use
 
 ## Implement https://github.com/icasimpan/docktie/issues/11
 ##           Add relative custom location of docker-compose.yml per project
@@ -41,17 +40,19 @@ DOCKERCOMPOSE_FULLPARENT_DIR="$(pwd)"
 [[ "$RELATIVE_DOCKERCOMPOSE_DIR" != "" ]] && DOCKERCOMPOSE_FULLPARENT_DIR="$(pwd)/${RELATIVE_DOCKERCOMPOSE_DIR}"
 
 ## Make sure docker-compose is installed since it's needed by the wrapper utils/docker-compose
-DOCKTIE_DOCKER_COMPOSE_BIN="$(which docker-compose 2>/dev/null)"
-if [[ "$DOCKTIE_DOCKER_COMPOSE_BIN" = "" ]]; then
-    echo '[ERROR] docker-compose not found. Please make sure to install it first.'
-    echo
-    exit 1
-else
-    export DOCKTIE_DOCKER_COMPOSE_BIN
+##
+if [[ "$DOCKTIE_INIT" != "true" ]]; then
+   DOCKTIE_DOCKER_COMPOSE_BIN="$(which docker-compose 2>/dev/null)"
+   if [[ "$DOCKTIE_DOCKER_COMPOSE_BIN" = "" ]]; then
+       echo '[ERROR] docker-compose not found. Please make sure to install it first.'
+       echo
+       exit 1
+   else
+       export DOCKTIE_DOCKER_COMPOSE_BIN
+   fi
 fi
 
-export DOCKTIE_DOCKER_COMPOSE_FULLPATH="${DOCKERCOMPOSE_FULLPARENT_DIR}/docker-compose.yml"
-if [[ "$(docker-compose -f ${DOCKTIE_DOCKER_COMPOSE_FULLPATH} ps | tail -n +2 | grep -cv exit)" -gt 0 ]]; then
+BOOTSTRAP_DOCKTIE() {
    if [[ "$DOCKTIE_INIT" = "true" ]]; then
       echo "ERROR: DockTie Dev Helper already initialized for '${PROJECT_NAME}'."
       echo "       Type 'exit' if you want to re-initialize."
@@ -69,13 +70,29 @@ if [[ "$(docker-compose -f ${DOCKTIE_DOCKER_COMPOSE_FULLPATH} ps | tail -n +2 | 
       echo "DockTie Dev Helper exited."
       echo
    fi
+} ## END: BOOTSTRAP_DOCKTIE()
+
+## Docker container(s) running...
+##
+export DOCKTIE_DOCKER_COMPOSE_FULLPATH="${DOCKERCOMPOSE_FULLPARENT_DIR}/docker-compose.yml"
+if [[ "$(docker-compose -f ${DOCKTIE_DOCKER_COMPOSE_FULLPATH} ps | tail -n +2 | grep -cv exit)" -gt 0 ]]; then
+  BOOTSTRAP_DOCKTIE
 
 ## Docker container(s) NOT running...
+##
 else
-   echo "ERROR: ${PROJECT_NAME} container(s) not running."
-   echo '       These commands might help:'
-   echo '          docker-compose up -d'
-   echo '          docker-compose start'
-   echo
-   exit 1
+   if [[ "$AUTOSTART_PROJECT" = "1" ]]; then
+      $DOCKTIE_DOCKER_COMPOSE_BIN up -d
+      BOOTSTRAP_DOCKTIE
+   else
+       echo "ERROR: ${PROJECT_NAME} container(s) not running."
+       echo '       These commands might help:'
+       echo '          docker-compose up -d'
+       echo '          docker-compose start'
+       echo
+       echo "       You may also try AUTOSTART_PROJECT=1 in ${DOCKTIE_ENV}"
+       echo '       if project can start without manual intervention.'
+       echo
+       exit 1
+   fi
 fi
