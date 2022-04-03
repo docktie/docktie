95b03bf9992baa5364e6eb31eb99e31a1960722b
Ismael Casimpan Jr <ismael.angelo@casimpan.com>
2022-04-03 19:28:46 +0800

    docktie/init.sh: Initial conversion to shcf per #22. utils is on a separate shcf.

--- init.sh
+++ init.sh
@@ -2,34 +2,29 @@
 ##
 #!/bin/bash
 
+## -------- source common library ---------
+SHCF_ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $SHCF_ROOTDIR/etc/controller.bash.inc
+
+## list of functions to auto-load
+functions_list="
+enforce_projectdir_docktie_init
+read_project_config
+bootstrap_docktie
+"
+autoload_functions "$functions_list"
+
 ## Enforce calling "init.sh" from within project directory.
 ##
-DOCKTIE_DIR=$(dirname $0)
-if [[ "$DOCKTIE_DIR" != "../docktie" ]]; then
-    echo "ERROR: 'init.sh' must be called from within project directory."
-    exit 1
-else
-    export DOCKTIE_COMMON_PARENTDIR="$(dirname $(pwd))"      ## for docktie-ext use
-    DOCKTIE_DIR="${DOCKTIE_COMMON_PARENTDIR}/docktie"
-fi
+enforce_projectdir_docktie_init
 
+export DOCKTIE_COMMON_PARENTDIR="$(dirname $(pwd))"      ## for docktie-ext use
+DOCKTIE_DIR="${DOCKTIE_COMMON_PARENTDIR}/docktie"
+
+PROJECT_ALIAS="$(basename $(pwd))"
 ## Read project config
 ##
-PROJECT_ALIAS=${1:-"$(basename $(pwd))"}
-
-CONF_DIR="$DOCKTIE_DIR/conf"
-ENV_FILE="$CONF_DIR/${PROJECT_ALIAS}_env"
-if [[ ! -e $ENV_FILE ]]; then
-    echo 'ERROR: Invalid parameter.'
-    echo 'Valid parameter(s) as follows:'
-    for alias_name in $(ls -1 $CONF_DIR/*_env|awk -F'/' '{print $NF}'|sed 's/_env$//g'); do
-        echo "  * $alias_name"
-    done
-    exit 1
-fi
-
-export DOCKTIE_ENV="$ENV_FILE"
-. $DOCKTIE_ENV
+read_project_config $PROJECT_ALIAS
 
 export PROJECT_ROOTDIR="${DOCKTIE_COMMON_PARENTDIR}/${PROJECT_ALIAS}" ## for docktie-ext use
 
@@ -52,38 +47,18 @@ if [[ "$DOCKTIE_INIT" != "true" ]]; then
    fi
 fi
 
-BOOTSTRAP_DOCKTIE() {
-   if [[ "$DOCKTIE_INIT" = "true" ]]; then
-      echo "ERROR: DockTie Dev Helper already initialized for '${PROJECT_NAME}'."
-      echo "       Type 'exit' if you want to re-initialize."
-      echo
-   else
-      export DOCKTIE_INIT=true
-      TOOLS_DIR=$DOCKTIE_DIR/utils/bin
-      echo "NOTE: DockTie Dev Helper initialized."
-      echo "      Utils($TOOLS_DIR) prioritized in \$PATH"
-      echo
-      export PATH=$TOOLS_DIR:$PATH
-      export DOCKER_CONTAINER_PREFIX=$DOCKER_CONTAINER_PREFIX  ## needed in ps1 change visibility done in next line
-      bash --rcfile <(cat $CONF_DIR/ps1_changer)
-      ## Below will run after 'exit' command
-      echo "DockTie Dev Helper exited."
-      echo
-   fi
-} ## END: BOOTSTRAP_DOCKTIE()
-
 ## Docker container(s) running...
 ##
 export DOCKTIE_DOCKER_COMPOSE_FULLPATH="${DOCKERCOMPOSE_FULLPARENT_DIR}/docker-compose.yml"
 if [[ "$(docker-compose -f ${DOCKTIE_DOCKER_COMPOSE_FULLPATH} ps | tail -n +2 | grep -cv exit)" -gt 0 ]]; then
-  BOOTSTRAP_DOCKTIE
+  bootstrap_docktie
 
 ## Docker container(s) NOT running...
 ##
 else
    if [[ "$AUTOSTART_PROJECT" = "1" ]]; then
       $DOCKTIE_DOCKER_COMPOSE_BIN up -d
-      BOOTSTRAP_DOCKTIE
+      bootstrap_docktie
    else
        echo "ERROR: ${PROJECT_NAME} container(s) not running."
        echo '       These commands might help:'
